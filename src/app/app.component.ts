422e348be2c513329d68e8dd82b8cd8f396ce2b3
bfv <dev@bfv.io>
2024-09-25 09:11:08 +0200

    added main page, header and menubar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from "./ui/toolbar/toolbar.component";
+import { MenubarComponent } from "./ui/menubar/menubar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent, MenubarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
