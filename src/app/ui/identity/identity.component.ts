562dfa91e20815c780c28577bb3779b14c067013
bfv <dev@bfv.io>
2024-09-25 13:26:42 +0200

    rough layout

--- src/app/ui/identity/identity.component.ts
+++ src/app/ui/identity/identity.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-identity',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './identity.component.html',
   styleUrl: './identity.component.scss'
 })
 export class IdentityComponent {
-
+  whoami: string = 'bronco';
 }
