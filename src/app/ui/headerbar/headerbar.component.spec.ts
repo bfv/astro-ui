62cef1e5da1732e38e8a912bedb5344ffe49bc31
bfv <dev@bfv.io>
2024-09-25 13:13:34 +0200

    renamed to headerbar, added identity

--- src/app/ui/headerbar/headerbar.component.spec.ts
+++ src/app/ui/headerbar/headerbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ToolbarComponent } from './toolbar.component';
+import { headerbarComponent } from './headerbar.component';
 
-describe('ToolbarComponent', () => {
-  let component: ToolbarComponent;
-  let fixture: ComponentFixture<ToolbarComponent>;
+describe('headerbarComponent', () => {
+  let component: headerbarComponent;
+  let fixture: ComponentFixture<headerbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToolbarComponent]
+      imports: [headerbarComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ToolbarComponent);
+    fixture = TestBed.createComponent(headerbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/ui/headerbar/headerbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IdentityComponent } from '../identity/identity.component';
+
+@Component({
+  selector: 'app-headerbar',
+  standalone: true,
+  imports: [ IdentityComponent ],
+  templateUrl: './headerbar.component.html',
+  styleUrl: './headerbar.component.scss'
+})
+export class headerbarComponent {
+
+}
