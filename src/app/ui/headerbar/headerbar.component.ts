9618a9662f6cdc3f375d022d8a5a0743ee4877db
bfv <dev@bfv.io>
2024-09-26 18:36:13 +0200

    added title, layout

--- src/app/ui/headerbar/headerbar.component.ts
+++ src/app/ui/headerbar/headerbar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { IdentityComponent } from '../identity/identity.component';
+import { TitleComponent } from '../title/title.component';
 
 @Component({
   selector: 'app-headerbar',
   standalone: true,
-  imports: [ IdentityComponent ],
+  imports: [ IdentityComponent, TitleComponent ],
   templateUrl: './headerbar.component.html',
-  styleUrl: './headerbar.component.scss'
+  styleUrl: './headerbar.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderbarComponent {
 
