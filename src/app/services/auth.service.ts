d2872d636311580130b855272988a30de87c720e
bfv <dev@bfv.io>
2024-09-26 18:30:24 +0200

    added loggedIn and whoami

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { computed, effect, Injectable, signal } from '@angular/core';
+import { Identity, nobody } from '../models/identity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private _loggedIn: boolean = false;
+  private _loggedInSignal = signal(false);
+  loggedIn = this._loggedInSignal.asReadonly();
+  
+  private _whoami = signal<Identity>(nobody);
+  whoami = this._whoami.asReadonly();
+
+  constructor() { 
+    effect(() => {
+      this._loggedIn = this._loggedInSignal();
+      console.log('auth effect: this._loggedIn =', this._loggedIn);
+    });
+  }
+
+  login() {
+    console.log('auth: login');
+    this._loggedInSignal.set(true);
+    this._whoami.set({ id: 5, name: 'bfv', email: 'dev@bfv.io' });	
+  }
+
+  logout() {
+    console.log('auth: logout');
+    this._loggedInSignal.set(false);
+    this._whoami.set(nobody);
+  }
 }
